9488c97ffb89351e8840bac61ad56a89bf77f113
kostyaprozhenik <kostyaprozhenik@gmail.com>
2022-05-23 22:56:11 +0300

    feat: complete copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const { promises: fs } = require('fs');
+const path = require('path');
 
+const dest = path.resolve(__dirname, 'files-copy');
+const src = path.resolve(__dirname, 'files');
+fs.rm(dest, { recursive: true, force: true })
+  .then(() => fs.mkdir(dest))
+  .then(() => fs.readdir(src))
+  .then((files) =>
+    Promise.all(
+      files.map((file) =>
+        fs.copyFile(path.join(src, file), path.join(dest, file))
+      )
+    )
+  );
